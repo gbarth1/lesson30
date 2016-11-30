228b2f9b1e4ea437885b85fa95ac8665e6e204f1
gbarth1 <grace.m.barth@gmail.com>
2016-11-30 14:18:08 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        StringBuffer sb=new StringBuffer();
+        //object created above
+	    String x="";
+        for(int i=0; i<=8;i++) {
+            sb.append("x");
+            System.out.println(sb);
+        }
+    }
+}
